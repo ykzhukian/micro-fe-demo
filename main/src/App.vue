c453e4536effebc12fcfff60044614ac89e8c1e8
Kian Zhu <kianzyk@gmail.com>
2020-10-28 11:52:12 +0800

    init

---
+++ main/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="main">
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+        <div class="logo" />
+        <a-menu theme="dark" mode="inline" :selectedKeys="[selected]" @select="handleChange">
+          <a-menu-item key="laravel">
+            <a-icon type="user" />
+            <span>Larave 项目</span>
+          </a-menu-item>
+          <a-menu-item key="vue3">
+            <a-icon type="video-camera" />
+            <span>Vue3 项目</span>
+          </a-menu-item>
+          <a-menu-item key="react">
+            <a-icon type="upload" />
+            <span>React 项目</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)"
+          />
+          <login />
+        </a-layout-header>
+        <a-layout-content
+          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+        >
+          {{selected}}
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+<script>
+import Login from './components/Login'
+
+export default {
+  data() {
+    return {
+      collapsed: false,
+      selected: 'laravel'
+    };
+  },
+  components: {
+    Login,
+  },
+  methods: {
+    handleChange({ key }) {
+      this.selected = key
+    }
+  },
+};
+</script>
+<style>
+.main section {
+  height: 100vh;
+}
+
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
---
+++ main/src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <span>
+    <a-button type="primary" @click="showModal" v-if="!login">
+      登录
+    </a-button>
+    <a-modal v-model="visible" title="登录平台" ok-text="确认" cancel-text="取消" @ok="handleLogin">
+      <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="handleLogin">
+        <a-form-item label="用户名">
+          <a-input
+            v-model="username"
+          />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input
+            type="password"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+    <span v-if="login">欢迎，{{username}}</span>
+  </span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      login: false,
+      visible: false,
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+    };
+  },
+  methods: {
+    showModal() {
+      this.visible = true;
+    },
+    hideModal() {
+      this.visible = false;
+    },
+    handleLogin() {
+      this.login = true
+      this.visible = false
+    }
+  },
+};
+</script>
